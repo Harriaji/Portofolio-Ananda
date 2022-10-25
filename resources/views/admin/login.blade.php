f44913a15ad077458875d50b2597e0c64a31decb
zenosamaaa <rezalol333@gmail.com>
2022-10-25 14:45:18 +0700

    commit seeder

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -20,6 +20,9 @@
     <!-- Custom styles for this template-->
     <link href="{{asset('template/css/sb-admin-2.min.css')}}" rel="stylesheet">
 
+    
+    {{-- App\Models\User::create(['name'=>'momo', 'email'=>'momo@gmail.com', 'password'=>bcrypt('hiraimomo') ]) --}}
+
 </head>
 
 <body class="bg-gradient-primary">
